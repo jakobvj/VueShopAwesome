533cb95cdab02be5eba91a3ac5d87b5f120e41b2
Jakob Varring Jensen <info@ioflow.dk>
2019-11-05 19:51:45 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    
+    <h2>Ecosystem</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  name: 'app',
+  data () {
+    
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  created(){
+    console.log('Created called')
+  }
+}
+</script>
+
+<style>
+</style>
